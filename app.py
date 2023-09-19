2e3dd382beda1ceb8067bd18fec12c2aed1868a2
dapt <diegodapt18@gmail.com>
2023-09-19 22:22:09 +0200

    updating app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def send(message):
     except Exception as e:
             bot.send_message(message.chat.id, f'Ocurrió un error: {str(e)}')
 
-'''
+
 @bot.message_handler(func = lambda message:True)
 def receive_response(message):
     response = message.text
@@ -33,19 +33,8 @@ def receive_response(message):
         pass
     else:
         pass
-    # bot.reply_to(message, f'{index} - did the rocket launch yet?')
-    bot.reply_to(message, message.text)
-'''
+    bot.reply_to(message, f'{1} - did the rocket launch yet?')
+
 
 bot.polling()
 
-# message confirm: f"{index} - did the rocket launch yet?"
-# message final: f"Found! Take-off = {bisector.index}"
-
-    # requests.get(f'https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20%28Hosted%20Webcast%29-wbSwFU6tY1c/frame/{index}/')
-    # print(message) # core.telegram.org/bots/api#sendPhoto
-    # requests.post(f'https://api.telegram.org/bot{bot_token}/sendPhoto', files=file)
-    # bot.send(image)
-    # bot.send_photo(6199381027, 'https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20%28Hosted%20Webcast%29-wbSwFU6tY1c/frame/39554/')
-    # with open('./theBotFather.png', 'rb') as photo:
-        # bot.send_photo(message.chat.id, photo)
